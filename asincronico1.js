8701d75a7a9af25af43490aa8eac95fbf3dded19
IgnaGonzAvi <103262229+IgnaGonzAvi@users.noreply.github.com>
2022-05-05 23:38:21 -0400

    Add files via upload

---
+++ asincronico1.js
@@ -0,0 +1,34 @@
+
+
+class Libro  {
+    constructor (nombreC, autorC, editorialC, generoC){
+
+    this.nombre = nombreC;
+    this.autor = autorC;
+    this.editorial = editorialC;
+    this.genero = generoC;
+}
+}
+function mostrarLibro(){
+    this.nombre, this.autor, this.editorial, this.genero
+
+}
+
+
+var metamorfosis = new Libro("La metamorfósis", "Franz Kafka", "Alfaguara", "Fantástico");
+var harry = new Libro("Harry Potter y la orden del fénix","J.K Rowling","Salamandra","Fantástico");
+var catedral = new Libro("La catedral del mar","Ildefonso Falcones","Alfaguara","Novela histórica");
+
+let libros = [metamorfosis, harry, catedral]
+libros [0] = metamorfosis;
+libros [1] = harry;
+libros [2] = catedral;
+
+
+console.log(libros);
+
+
+
+
+
+
