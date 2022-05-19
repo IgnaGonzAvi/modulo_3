0d8fd82fcd3e4ad7f1c4e2a5edf057f7d43c23e4
IgnaGonzAvi <103262229+IgnaGonzAvi@users.noreply.github.com>
2022-05-19 00:57:35 -0400

    Add files via upload

---
+++ asincronico2.js
@@ -0,0 +1,21 @@
+
+let date = new Date();
+var dia = date.getDay()
+
+if (dia == 1){
+    alert("Hoy es lunes");
+}else if(dia ==2){
+    alert("Hoy es martes");
+}else if(dia == 3){
+    alert("Hoy es miércoles");
+}else if(dia == 4){
+    alert("Hoy es jueves");
+}else if(dia == 5){
+    alert("Hoy es viernes");
+}else if(dia == 6){
+    alert("Hoy es sábado");
+}else{
+    alert("Hoy es domingo");
+}
+
+
